ccb73aae0ef49e9d0220bea42bf6e1322ca020d4
Rickard <rickard@elliot.id>
2025-04-16 20:59:03 +0200

    Updated Prefect flow

--- prefect_pipeline.py
+++ prefect_pipeline.py
@@ -1,7 +1,5 @@
 from prefect import task, flow
-from prefect_github.repository import GitHubRepository
-from prefect_github import GitHubCredentials
-from datetime import timedelta
+from prefect_github import GitHubCredentials, GitHubRepository
 import subprocess
 
 # Define the task to build and run Docker container
@@ -21,17 +19,15 @@ def build_and_run_pipeline():
     docker_task = build_and_run_docker()
     run_dbt(upstream_tasks=[docker_task])
 
-# Load the GitHub blocks
-github_repository_block = GitHubRepository.load("githubrepository")  # Replace with your actual block name
+# Load the GitHub blocks (These blocks were created in Prefect Cloud)
+github_repository_block = GitHubRepository.load("githubrespoitory")  # Replace with your actual block name
 github_credentials_block = GitHubCredentials.load("githubtoken")  # Replace with your actual block name
-
-# Use the GitHub repository and credentials block
-source = github_repository_block.get_source()  # Get the repository source from the GitHub block
 
 # Deploy the flow from the GitHub repo
 build_and_run_pipeline.from_source(
-    source=source,
+    source="https://github.com/Rickarddev-code/zoomcamp-project.git",  # Replace with your GitHub repository URL
     entrypoint="prefect_pipeline.py:build_and_run_pipeline",  # The entry point is your prefect_pipeline.py file
 ).deploy(
-    name="build-and-run-pipeline",  # Name of the deployment in Prefect Cloud
+    name="build-and-run-pipeline-v2",  # Name of the deployment in Prefect Cloud
+    work_pool_name="cloud_deploy",  # Specify the work pool name here
 )
